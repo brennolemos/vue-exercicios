80605ad94859e460126c07807193775b59b842bf
Breno Lemos <brenomiros@gmail.com>
2019-03-04 11:48:48 -0300

    Criar projeto usando vue-cli

---
+++ curso-vue/componentes/componente-intro/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<div id="app">
+  <h1>Contadores</h1>
+  <h2> {{ contador }}</h2>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contador: 5
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
