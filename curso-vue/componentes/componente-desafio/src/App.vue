476c8c937c363a0bd3897919958006a3c4c9fd94
Breno Lemos <brenomiros@gmail.com>
2019-03-05 15:18:22 -0300

    Desafio - Organizar projeto em Componentes

---
+++ curso-vue/componentes/componente-desafio/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<v-app>
+		<app-menu />
+		<toolbar />
+		<carousel />
+		<app-footer />
+		
+	</v-app>
+</template>
+
+<script>
+import Menu from './Menu.vue'
+import Toolbar from './Toolbar.vue'
+import Carousel from './Carousel.vue'
+import Footer from './Footer.vue'
+
+export default {
+	components: {
+		'app-menu': Menu,
+		toolbar: Toolbar,
+		carousel: Carousel,
+		'app-footer': Footer
+	}
+}
+</script>
