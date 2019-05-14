84508562dd5b0706a29981b8fce94e481022b799
Breno Lemos <brenomiros@gmail.com>
2019-05-13 22:02:53 -0300

    Transition 1

---
+++ curso-vue-origamid/transition/transition.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button @click="ativo = !ativo">Ativar</button>
+    <transition name="fade">
+      <h1 v-if="ativo">Seriços</h1>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ativo: false
+    };
+  }
+};
+</script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity .3s;
+}
+.fade-enter,
+.fade-leave-to {
+    opacity: 0;
+}
+</style>
