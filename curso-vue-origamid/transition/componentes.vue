6488a808167cbf5d531dd6d89b5a9497901d09c8
Breno Lemos <brenomiros@gmail.com>
2019-05-18 15:01:33 -0300

    Components

---
+++ curso-vue-origamid/transition/componentes.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button @click="estoque++">Adicionar Estoque</button>
+    <button @click="estoque = 0">Deletar Estoque</button>
+    <transition mode="out-in">
+      <p v-if="estoque > 0" key="estoque">{{estoque}} Itens em estoque</p>
+      <p v-else key="esgotado">Produto fora de estoque</p>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      estoque: 0
+    };
+  }
+};
+</script>
+
+<style>
+
+.v-enter-active,
+.v-leave-active {
+    transition: opacity .3s;
+}
+
+.v-enter,
+.v-leave-to {
+    opacity: 0;
+}
+
+</style>
