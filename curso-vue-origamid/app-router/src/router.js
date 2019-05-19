d97f9f85f650c6389f565689b9e98f9eee942ca7
Breno Lemos <brenomiros@gmail.com>
2019-05-19 10:46:08 -0300

    Router

---
+++ curso-vue-origamid/app-router/src/router.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Cursos from './views/Cursos.vue';
+
+Vue.use(Router);
+
+export default new Router({
+    base: 'app',
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            component: Home
+        },
+        {
+            path: '/cursos',
+            component: Cursos
+        },
+    ]
+})
---
+++ curso-vue-origamid/app-router/src/views/Cursos.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2>Essa é a página Cursos</h2>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
